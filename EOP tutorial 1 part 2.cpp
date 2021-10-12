80557ae2d0d0a7dd442454769a28803141386bcd
Nursyakirah Ridzuan <nursyakirah.ridzuan@live.iium.edu.my>
2021-10-12 21:51:06 +0800

    first tuto class

---
+++ EOP tutorial 1 part 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a;
+	int b;
+	int sum;
+	
+	cout << "Insert the value for a: \n";
+	cin >> a;
+	cout << "Insert the value for b: \n";
+	cin >> b;
+	
+	sum = a+b;
+	cout << a << " + " << b << " = " << sum;
+	
+	return 0;
+}
---
+++ EOP tutorial 1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	const int a = 4;
+	int z, x, c;
+	int character = 'A'
+	float t= 4.34;
+	double y= 4.34;
+	char i = 'a';
+	string name = "Syak" ;
+	bool statement = true;
+	bool lies = false;
+	
+/*	cout << "Insert a number \n";
+	cin >> a;
+	cout << "Number inserted: " << a;
+  
+    cout << "float: " << t;
+    cout << "\ndouble: " << y;
+    
+    cout << "Char: " << i;
+    cout << "\nString: " << name;
+
+    cout << "Statement: " << statement;
+    cout << "\nLies: " << lies;
+*/
+    z=1;
+    x=2;
+    c=3;
+    cout << z << "\n";
+    cout << x << "\n";
+    cout << c;
+    
+	return 0;
+}
